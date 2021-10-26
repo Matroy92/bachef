b7426418dee77a70b29a5ba0a739294afb2979d2
Zakaria LAMJOUN <lamjounz@gmail.com>
2021-10-26 20:44:02 +0200

    scrapping de titre de recette

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,10 +1,27 @@
-
+require "pry-byebug"
 require 'open-uri'
 require 'nokogiri'
 
 class GamesController < ApplicationController
-   def index
+  def test_scrap
+    if params[:query].present?
+      url = "https://www.marmiton.org/recettes/recherche.aspx?aqt=#{params[:query]}" #ok
+      html_file = URI.open(url).read
+      html_doc = Nokogiri::HTML(html_file)
+      html_doc.search('.MRTN__sc-30rwkm-0.dJvfhM').each do |element|
+        #puts "\n\n\n#{element.text.strip}\n\n\n"
+        #puts element.text.strip
+        @test = element.text.strip
+        
+        
+        
+      end
+      
+    end
+  end
 
-   end
+  def index
+    
+  end
 
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -24,7 +24,7 @@ require("channels")
 
 // External imports
 import "bootstrap";
-import {searchInput} from '../plugins/api_marmiton';
+
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
@@ -32,5 +32,5 @@ import {searchInput} from '../plugins/api_marmiton';
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
-  searchInput();
+  
 });
