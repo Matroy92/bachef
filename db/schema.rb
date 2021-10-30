7f6c7787bf623b9c09dbec2db25bbb1de78c71b1
Zakaria LAMJOUN <lamjounz@gmail.com>
2021-10-30 14:47:42 +0200

    commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_30_120241) do
+ActiveRecord::Schema.define(version: 2021_10_30_114733) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,7 +31,6 @@ ActiveRecord::Schema.define(version: 2021_10_30_120241) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "premium"
-    t.integer "price"
     t.integer "price_cents", default: 0, null: false
   end
 
